03826725aa212cd51178315f2e295b3365350574
AbishakParthi <abishakeparthi4@gmail.com>
2026-06-23 19:49:05 +0530

    Resume Update

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -118,7 +118,7 @@ export const ContactSection = () => {
             <div className="pt-8">
               <h4 className="font-medium mb-4"> Connect With Me</h4>
               <div className="flex space-x-4 justify-center">
-                <a href="linkedin.com/in/abishake-parthi" target="_blank">
+                <a href="https://linkedin.com/in/abishake-parthi" target="_blank">
                   <Linkedin />
                 </a>
                 <a href="#" target="_blank">
